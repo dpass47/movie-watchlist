11211bb0f7d791e7414765c5fd6dab704d3935be
dpass47 <105099652+dpass47@users.noreply.github.com>
2022-06-09 22:42:29 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,58 @@
+const searchButton = document.querySelector(".search-btn");
+const searchInput = document.querySelector(".search-input");
+const movieContentHtml = document.querySelector(".movie-content");
+const searchForm = document.querySelector(".search-form");
+var movieListArr = [];
+var localMovieArr = [];
+var movieArrLocalStorage;
+
+async function handleClick() {
+	const res = await fetch(
+		`http://www.omdbapi.com/?apikey=TEST&s=${searchInput.value}&type=movie`
+	);
+	const data = await res.json();
+	movieListArr = data.Search.slice(0, 5);
+	movieContentHtml.innerHTML = "";
+	postData();
+}
+
+async function postData() {
+	for (var i = 0; i < movieListArr.length; i++) {
+		const res = await fetch(
+			`http://www.omdbapi.com/?apikey=TEST&t=${movieListArr[i].Title}`
+		);
+		const movieData = await res.json();
+		movieContentHtml.innerHTML += `<div class="movie">
+				<img src="${movieData.Poster}">
+				<div class="movie-data">
+				<h2 class="movie-title">${movieData.Title}</h2>
+				<p><span class="run-time-text">${movieData.Runtime}</span><span class="genre-text">${movieData.Genre}</span><button class="watchlist-add-btn">Watchlist</button></p>
+				<p class="plot-text">${movieData.Plot}</p>
+				</div>	
+				</div>`;
+	}
+	const watchlistBtns = document.querySelectorAll(".watchlist-add-btn");
+	for (let i = 0; i < movieListArr.length; i++) {
+		watchlistBtns[i].addEventListener("click", callback(i));
+		watchlistBtns[i].addEventListener("click", function () {
+			watchlistBtns[i].innerHTML = "Added to watchlist!";
+			watchlistBtns[i].style.backgroundImage = "none";
+			watchlistBtns[i].style.cursor = "default";
+			watchlistBtns[i].disabled = true;
+		});
+	}
+}
+
+searchForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	handleClick();
+});
+
+function callback(x) {
+	return function addEntry() {
+		var exisiting = JSON.parse(localStorage.getItem("movieArr"));
+		if (exisiting == null) exisiting = [];
+		exisiting.push(movieListArr[x]);
+		localStorage.setItem("movieArr", JSON.stringify(exisiting));
+	};
+}
---
+++ js/watchlist.js
@@ -0,0 +1,53 @@
+const movieContentHtml = document.querySelector(".movie-content");
+const moviePlacehold = document.querySelector(".movie-placehold");
+var movieArr;
+
+function getData() {
+	movieArr = JSON.parse(localStorage.getItem("movieArr"));
+}
+
+getData();
+
+function clearData() {
+	if (movieArr.length >= 1) {
+		moviePlacehold.style.display = "none";
+	} else {
+		moviePlacehold.style.display = "flex";
+	}
+}
+
+clearData();
+
+async function postData() {
+	movieContentHtml.innerHTML = "";
+	for (var i = 0; i < movieArr.length; i++) {
+		const res = await fetch(
+			`http://www.omdbapi.com/?apikey=TEST&t=${movieArr[i].Title}`
+		);
+		const movieData = await res.json();
+		movieContentHtml.innerHTML += `<div class="movie">
+				<img src="${movieData.Poster}">
+				<div class="movie-data">
+				<h2 class="movie-title">${movieData.Title}</h2>
+				<p><span class="run-time-text">${movieData.Runtime}</span><span class="genre-text">${movieData.Genre}</span><button class="watchlist-remove-btn">Remove</button></p>
+				<p class="plot-text">${movieData.Plot}</p>
+				</div>	
+				</div>`;
+	}
+	const watchlistRemoveBtns = document.querySelectorAll(
+		".watchlist-remove-btn"
+	);
+	for (let i = 0; i < movieArr.length; i++) {
+		watchlistRemoveBtns[i].addEventListener("click", function () {
+			var exisiting = JSON.parse(localStorage.getItem("movieArr"));
+			if (exisiting == null) exisiting = [];
+			exisiting.splice(i, 1);
+			movieArr.splice(i, 1);
+			localStorage.setItem("movieArr", JSON.stringify(exisiting));
+			postData();
+			clearData();
+		});
+	}
+}
+
+postData();
